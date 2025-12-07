bd8471b0525c2e6dd2dee028789817810efddc11
Swarnavo2003 <swarnavo2020@gmail.com>
2025-12-07 16:03:58 +0530

    feat: grocery model added

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+export const ROLES: string[] = ["user", "deliveryBoy", "admin"];
+
+export const CATEGORIES: string[] = [
+  "Fruits & Vegetables",
+  "Dairy & Eggs",
+  "Rice, Atta & Grains",
+  "Snacks & Biscuits",
+  "Spices & Masalas",
+  "Beverages & Drinks",
+  "Personal Care",
+  "Household Essentials",
+  "Instant & Packaged Food",
+  "Baby & Pet Care",
+];
---
+++ src/models/grocery.model.ts
@@ -0,0 +1,32 @@
+import { CATEGORIES } from "@/constants";
+import mongoose from "mongoose";
+
+export interface IGrocery {
+  _id?: mongoose.Types.ObjectId;
+  name: string;
+  category: string;
+  price: string;
+  unit: string;
+  image: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const grocerySchema = new mongoose.Schema<IGrocery>(
+  {
+    name: { type: String, required: true },
+    category: {
+      type: String,
+      enum: CATEGORIES,
+      required: true,
+    },
+    price: { type: String, required: true },
+    unit: { type: String, required: true },
+    image: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Grocery =
+  mongoose.models.Grocery || mongoose.model("Grocery", grocerySchema);
+export default Grocery;
